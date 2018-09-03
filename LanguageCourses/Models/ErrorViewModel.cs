0d73f916ea8e1f918f6477aa23774e192fc7bd52
Ostap <moravsky@itwconsulting.com>
2018-09-03 17:05:32 +0300

    Add project files.

---
+++ LanguageCourses/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LanguageCourses.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
